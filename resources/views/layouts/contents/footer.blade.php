93343a9ded6655f71589d16c7ef50b85d1639f9c
lutf87 <luthfiarrahman007@gmail.com>
2022-12-27 15:50:34 +0700

    auth

--- resources/views/layouts/contents/footer.blade.php
+++ resources/views/layouts/contents/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-<footer class="bg-dark text-center text-white mt-4">
+<footer class="bg-dark text-center text-white mt-4 fixed-bottom">
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
         © 2020 Copyright:
--- resources/views/layouts/contents/navbar.blade.php
+++ resources/views/layouts/contents/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
+    <div class="container">
         <a class="navbar-brand" href="#">Navbar</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -25,7 +25,7 @@
             </ul>
             <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Login</a>
+                    <a class="nav-link" href="{{ route('login') }}">Login</a>
                 </li>
             </ul>
         </div>
